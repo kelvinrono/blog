217f225979d02194c3d18d5e4ca7ddd94426cb88
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-19 15:41:46 +0300

    set up a form

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+class PitchForm(FlaskForm):
+
+    title = StringField('Pitch title',validators=[Required()])
+    text = TextAreaField('Text',validators=[Required()])
+    category = SelectField('Type',choices=[('market','Market pitch'),('business','Business pitch'),('sales','Sales pitch')],validators=[Required()])
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Bio.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    text = TextAreaField('Leave a comment:',validators=[Required()])
+    submit = SubmitField('Submit')
